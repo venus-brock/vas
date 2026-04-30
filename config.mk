cad6ad0a61ab82a93d3d460ad938a5348d120121
venus brock <venus@brock-v.dev>
2026-04-30 11:23:44 -0500

    silence unused parameter compiler warning

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@ PREFIX = /usr
 INCS = -I/usr/include/jack -I/usr/include/X11
 LIBS = -ljack -lX11 -lm
 
-CFLAGS = -std=c99 -D_XOPEN_SOURCE=500 -Wall -Wextra -pedantic -Wno-unused-function ${INCS}
+CFLAGS = -std=c99 -D_XOPEN_SOURCE=500 -Wall -Wextra -pedantic -Wno-unused-function -Wno-unused-parameter ${INCS}
 LDFLAGS = ${LIBS}
 
 SRC = src/vas.c src/vas.h src/preset.c src/vas_gui.c
